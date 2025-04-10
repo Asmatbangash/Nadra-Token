4d80338acded9b51038a485bf31c1a0cdf23a3aa
Asmatbangash <bangashasmat877@gmail.com>
2025-04-10 10:30:32 +0500

    modify db connection

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv'
 import ConnectedDb from './db/db.connection.js'
+import { app } from './app.js'
 
 dotenv.config()
+const port = process.env.PORT || 8080;
 
 
-ConnectedDb()
+ConnectedDb().then(()=> app.listen(port, () => {
+console.log(`server is listning on port ${port}`)
+})).catch((err) => console.log('database connection failed!', err))
