8aca48f157084d41f8a78306c3ede3a9663bf05b
Asmatbangash <bangashasmat877@gmail.com>
2025-04-15 15:52:20 +0500

    add !

--- Backend/src/db/db.connection.js
+++ Backend/src/db/db.connection.js
@@ -6,9 +6,9 @@ const ConnectedDb = async () =>{
   console.log(`${process.env.MONGODB_URL}/${Db_Name}`)
     try{
       await mongoose.connect(`${process.env.MONGODB_URL}/${Db_Name}`)
-      console.log('database successfully connected')
+      console.log('database successfully connected!')
     }catch(error){
-        console.log('database connection faild', error)
+        console.log('database connection faild!', error)
     }
 }
 
