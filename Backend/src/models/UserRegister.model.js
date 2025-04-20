ee1bb0cf53e8c0b5d2d2d438a4b9e35f54617434
Asmatbangash <bangashasmat877@gmail.com>
2025-04-20 09:22:51 +0500

    installing of jwt and  add generateAccessToekn and generateRefreshToekn methods for user signIn

--- Backend/src/models/UserRegister.model.js
+++ Backend/src/models/UserRegister.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
     userName:{
@@ -35,6 +36,33 @@ userSchema.pre("save", function(next){
 
 userSchema.methods.isPasswordCorrect = async function(password){
   return await  bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        userName: this.userName,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRETE,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRETE,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 const User = mongoose.model("User", userSchema)
